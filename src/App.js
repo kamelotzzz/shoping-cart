5d2a05b573d5bd9af166637be1a580653691eed3
KhuongNv <dev18@lifetek.vn>
2020-08-28 18:40:50 +0700

    1231

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
+import data from "./data.json";
+import Products from "./component/Products";
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/"> React Shoping Cart</a>
-      </header>
-      <main>Product List</main>
-      <footer>All right is reserved</footer>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/"> React Shoping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products}></Products>
+            </div>
+            t<div className="sidebar">Cart Items</div>
+          </div>
+        </main>
+        <footer>All right is reserved</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
