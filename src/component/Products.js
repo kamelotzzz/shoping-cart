392a8d1e82d4e0c6ea15fdf3c031b81c7d5926a2
tuyennt <dev07@lifetek.vn>
2020-09-01 15:28:20 +0700

    1-09 commit

--- src/component/Products.js
+++ src/component/Products.js
@@ -1,20 +1,27 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import formatCurrency from '../ulti';
 
 class Products extends Component {
   render() {
     return (
       <div>
-        <ul className="products">
+        <ul className='products'>
           {this.props.products.map((product) => (
             <li key={product._id}>
-              <div className="product">
-                <a href={"#" + product._id}>
+              <div className='product'>
+                <a href={'#' + product._id}>
                   <img src={product.image} alt={product.title}></img>
                   <p>{product.title}</p>
                 </a>
-                <div className="product-price">
-                  <div>{product.price}</div>
-                  <button className="button primary"> Add To Cart</button>
+                <div className='product-price'>
+                  <div>{formatCurrency(product.price)}</div>
+                  <button
+                    onClick={() => this.props.addToCart(product)}
+                    className='button primary'
+                  >
+                    {' '}
+                    Add To Cart
+                  </button>
                 </div>
               </div>
             </li>
